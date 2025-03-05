b27a31a9c67d5b2507ac46487eaa328146a296f0
VitaliySlivchuk <41226137+VitaliySlivchuk@users.noreply.github.com>
2025-03-05 18:58:19 +0100

    modal with animation

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,40 @@
 import { useState } from "react";
 import "./modal.scss";
 
+const ModalWindow = ({ open, setOpen, children }) => (
+  <div className={`overlay animated ${open ? "show" : ""}`}>
+    <div className="modal">
+      <svg
+        onClick={() => setOpen(false)}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      {children}
+    </div>
+  </div>
+);
+
 export const Modal = () => {
   const [open, setOpen] = useState(false);
 
-  const onClickOpenModal = () => {
-    setOpen(!open);
-  };
-
   return (
     <div className="Modal">
-      <button onClick={onClickOpenModal} className="open-modal-btn">
-        ✨ Открыть окно
+      <button
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="open-modal-btn"
+      >
+        ✨ Open wimdow
       </button>
-      {open && (
-        <div className="overlay">
-          <div className="modal">
-            <svg
-              onClick={onClickOpenModal}
-              height="200"
-              viewBox="0 0 200 200"
-              width="200"
-            >
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-          </div>
-        </div>
-      )}
+      <ModalWindow open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      </ModalWindow>
+      {/* {open && <ModalWindow open={open} setOpen={setOpen} />} */}
     </div>
   );
 };
